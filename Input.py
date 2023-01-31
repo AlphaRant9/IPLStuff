2a0d1f50b5358c93c9e018cdb4882a739daeb28d
AlphaRant9 <ethan.putz3.14@gmail.com>
2023-01-31 12:01:49 -0600

    31.01.2023 - Implemented RPS into input.py

--- Input.py
+++ Input.py
@@ -1,4 +1,64 @@
-from random import *
+import math;
+from random import *;
+import random;
+from math import *;
+
+rock = 1;
+paper = 2;
+scissors = 3;
+
+def whoWon(userNum, rps, userStr, score, compScore):
+    if (rps == 1):
+        compStr = "Rock";
+    elif (rps == 2):
+        compStr = "Paper";
+    else:
+        compStr = "Scissors"
+    gameDiff = userNum - rps;
+    if (gameDiff == 1 or gameDiff == -2):
+        score += 1;
+        print("Good Job!\nComputer Picked: " + compStr + "\nYou picked: " + userStr +  "\nYour score is now " + str(score) + ".\nThe Computer's Score is now " + str(compScore) + ".");
+        RPS(score, compScore);
+    elif (gameDiff == -1 or gameDiff == 2):
+        compScore += 1;
+        print("Nice Try!\nComputer Picked: " + compStr + "\nYou picked: " + userStr +  "\nYour score is now " + str(score) + ".\nThe Computer's Score is now " + str(compScore) + ".");
+        RPS(score, compScore);
+    elif (gameDiff == 0):
+        print("Tie!\nYour Score: " + str(score) + "\nThe Computer's Score is: " + str(compScore));
+        RPS(score, compScore);
+
+def RPS(localScore, compScoreIn):
+    score = localScore;
+    compScore = compScoreIn;
+    rps = randint(1,3);
+    userNum = 0;
+    
+    if (score > compScore):
+        endstr = "You Won!";
+    elif (score < compScore):
+        endstr = "You Lost!"
+    else:
+        endstr = "Tie!";
+    
+    usersel = input("Rock, Paper, or Scissors? ");
+    if (usersel.lower() == "rock" or usersel.lower() == "r"):
+        userNum = 1;
+        userStr = "Rock";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel.lower() == "paper" or usersel.lower() == "p"):
+        userNum = 2;
+        userStr = "Paper";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel.lower() == "scissors" or usersel.lower() == "s"):
+        userNum = 3;
+        userStr = "Scissors";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel == "end"):
+        print(endstr + "\nThanks for playing!\nYour Final Score was " + str(score) + ".\nThe Computer's Final Score was " + str(compScore));
+        start();
+    else:
+        print("ERROR: You must input Rock, r, Paper, p, Scissors, or s.\nIf you wish to end the game simply type end.");
+        RPS(score, compScore);
 
 def ghostGame(localScore):
     score = localScore
@@ -60,8 +120,10 @@ def start():
     elif (usage.lower() == "gg" or usage.lower() == "ghost game" or usage.lower() == "ggame" or usage.lower() == "ghostgame"):
         print("Putz Ghost Game.")
         ghostGame(0)
+    elif (usage.lower() == "rps" or usage.lower() == "rockpaperscissors" or usage.lower == "rock paper scissors"):
+        RPS(0,0);
     else:
-        print("Invalid Tool. Options are:\nCalculator - 'calc' or 'calculator\nGhost Game - 'gg' or 'ghost game' or 'ggame' or 'ghostgame'")
+        print("Invalid Tool. Options are:\nCalculator - 'calc' or 'calculator'\nGhost Game - 'gg' or 'ghost game' or 'ggame' or 'ghostgame'\nRock Paper Scissors - 'rps' or 'rock paper scissors' or 'rockpaperscissors'")
         start()
 
 
--- PutzRPS.py
+++ PutzRPS.py
@@ -7,29 +7,13 @@ rock = 1;
 paper = 2;
 scissors = 3;
 
-def RPS(localScore, compScoreIn):
-    score = localScore;
-    compScore = compScoreIn;
-    rps = randint(1,3);
-    userNum = 0;
-    userStr = "";
+def whoWon(userNum, rps, userStr, score, compScore):
     if (rps == 1):
         compStr = "Rock";
     elif (rps == 2):
         compStr = "Paper";
     else:
         compStr = "Scissors"
-    usersel = input("Rock, Paper, or Scissors? ");
-    if (usersel.lower() == "rock" or usersel.lower() == "r"):
-        userNum = 1;
-    elif (usersel.lower() == "paper" or usersel.lower() == "p"):
-        userNum = 2;
-    elif (usersel.lower() == "scissors" or usersel.lower() == "s"):
-        userNum = 3;
-    else:
-        print("ERROR: You must input Rock, r, Paper, p, Scissors, or s.\nIf you wish to end the game simply type end.");
-        RPS(score);
-
     gameDiff = userNum - rps;
     if (gameDiff == 1 or gameDiff == -2):
         score += 1;
@@ -41,7 +25,40 @@ def RPS(localScore, compScoreIn):
         RPS(score, compScore);
     elif (gameDiff == 0):
         print("Tie!\nYour Score: " + str(score) + "\nThe Computer's Score is: " + str(compScore));
-    elif usersel == "end":
-        print("Thanks for playing! Your Final Score was " + str(score));
+        RPS(score, compScore);
 
+def RPS(localScore, compScoreIn):
+    score = localScore;
+    compScore = compScoreIn;
+    rps = randint(1,3);
+    userNum = 0;
+    
+    if (score > compScore):
+        endstr = "You Won!";
+    elif (score < compScore):
+        endstr = "You Lost!"
+    else:
+        endstr = "Tie!";
+    
+    usersel = input("Rock, Paper, or Scissors? ");
+    if (usersel.lower() == "rock" or usersel.lower() == "r"):
+        userNum = 1;
+        userStr = "Rock";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel.lower() == "paper" or usersel.lower() == "p"):
+        userNum = 2;
+        userStr = "Paper";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel.lower() == "scissors" or usersel.lower() == "s"):
+        userNum = 3;
+        userStr = "Scissors";
+        whoWon(userNum, rps, userStr, score, compScore);
+    elif (usersel == "end"):
+        print(endstr + "\nThanks for playing!\nYour Final Score was " + str(score) + ".\nThe Computer's Final Score was " + str(compScore));
+    else:
+        print("ERROR: You must input Rock, r, Paper, p, Scissors, or s.\nIf you wish to end the game simply type end.");
+        RPS(score, compScore);
+
+    
+    
 RPS(0, 0);
